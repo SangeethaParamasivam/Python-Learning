ff5f1a63be0bdf091084178336c87367b6e9efe2
Sangeetha P R <prsangeetha1999@gmail.com>
2025-03-21 01:10:38 +0530

    userinp-tuple.py

---
+++ userinp-tuple.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+
+age = int(input("Enter your age: "))
+
+gender = input("Enter your gender: ")
+
+city = input("Enter your city: ")
+
+japanese_lvl = input("Enter your Japanese level: ")
+
+
+
+Tuple = name, age, gender, city, japanese_lvl
+#Tuple=(name,age,gender,city,japanese_evel)
+
+
+print(Tuple)
