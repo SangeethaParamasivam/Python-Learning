ab04b81939a1c485e2c0f6fc8c05b52a6c638194
Sangeetha P R <prsangeetha1999@gmail.com>
2025-03-21 00:59:24 +0530

    even-numbers.py

---
+++ even-numbers.py
@@ -0,0 +1,9 @@
+myList = []   #Creating an empty list
+
+for number in range(201):  #For loop 
+
+    if number % 2 == 0:   #Condition checking for even numbers
+
+        myList.append(number)  #Adding the elements to the list
+
+print(myList)
