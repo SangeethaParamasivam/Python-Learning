486b48f0cc0fd92a83c84694c257cf950d866477
Sangeetha P R <prsangeetha1999@gmail.com>
2025-03-21 08:14:07 +0530

    dictionary-biodata.py

---
+++ dictionary-biodata.py
@@ -0,0 +1,12 @@
+# Creating a dictionary to store bio data
+bio_data = {
+    'Name': 'Sangeetha',
+    'Age': 24,
+    'City': 'Bangalore',
+    'Level': 'N3'
+}
+
+# Printing the bio data
+print("\nBio Data:")
+for key, value in bio_data.items():
+    print(f"{key}: {value}")
