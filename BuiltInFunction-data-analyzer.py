4a6450bbc887381fc444be040a027825429ae7fb
Sangeetha P R <prsangeetha1999@gmail.com>
2025-04-03 06:27:01 +0530

    BuiltInFunction-data-analyzer.py

---
+++ BuiltInFunction-data-analyzer.py
@@ -0,0 +1,15 @@
+'''*Built-in Function Assignment Question*
+
+1. Write a function called data_analyzer that processes a list of numeric values and returns a dictionary with the following information:
+
+    -> The sum of all values (using the appropriate built-in function)
+    -> The minimum and maximum values (using appropriate built-in functions)
+    -> The average (mean) of all values (calculate this using built-in functions)
+    -> The number of values that are above the average
+    -> A sorted version of the original list (using the appropriate built-in function)
+
+2. Write a function called transform_data that takes a list of numbers and performs these operations:
+
+    -> Filter out all negative numbers (using filter())
+    -> Square each remaining number (using map())
+    -> Calculate the product of all resulting values (using functools.reduce()) '''
