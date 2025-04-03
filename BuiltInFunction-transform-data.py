8cfbe536728abb9d10e6d24bbd1f9c4618d20f2a
Sangeetha P R <prsangeetha1999@gmail.com>
2025-04-03 06:40:22 +0530

    BuiltInFunction-transform-data.py

---
+++ BuiltInFunction-transform-data.py
@@ -0,0 +1,7 @@
+''' Built-in-function-question:
+2. Write a function called transform_data that takes a list of numbers and performs these operations:
+
+    -> Filter out all negative numbers (using filter())
+    -> Square each remaining number (using map())
+    -> Calculate the product of all resulting values (using functools.reduce()) ''' 
+
