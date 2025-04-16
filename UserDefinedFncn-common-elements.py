a5b23eff588a990971767f4bad47e4085428eead
Sangeetha P R <prsangeetha1999@gmail.com>
2025-04-16 20:36:02 +0530

    UserDefinedFncn-common-elements.py

--- UserDefinedFncn-common-elements.py
+++ UserDefinedFncn-common-elements.py
@@ -1,18 +1,8 @@
 '''*User - defined function assignment question* 
 
 2. Write a function find_common_elements(list1, list2) that returns a list of common elements between two lists.'''
-def find_x(s):
+def find_common_elements(list1, list2):
 
-    pos = -1
+    return list(set(list1) & set(list2))
 
-    for i in range(len(s)):
-
-        if s[i] == 'x':
-
-            pos = i
-
-            break
-
-    return pos
-
-print(find_x("The experimental xenon exhaust extractor exceeded expectations in extreme x-ray exposure experiments"))
+print(find_common_elements([1, 2, 3, 4], [3, 4, 5, 6]))  
