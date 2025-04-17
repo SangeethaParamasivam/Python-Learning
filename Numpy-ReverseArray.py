895c1a5b5b64c5fac809797b6e24a40aadf7133a
Sangeetha P R <prsangeetha1999@gmail.com>
2025-04-17 07:36:32 +0530

    Numpy-ReverseArray.py

---
+++ Numpy-ReverseArray.py
@@ -0,0 +1,17 @@
+'''*Numpy arrays assignment questions*
+1. Given an array arr = np.array([1, 2, 3, 4, 5]), convert all values to float
+2. Write a program to reverse a NumPy array.
+3. Create a 4x4 array with random integers between 1 and 100. Find the maximum value in each row
+
+2. Write a program to reverse a NumPy array.'''
+
+import numpy as np
+
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+rev_arr = arr[::-1]
+
+
+
+print(rev_arr)
